ff517dd8c33b6fdb71b8069aa23ecf35336bdd0b
Parfenovx1 <parfenovx1@gmail.com>
2020-04-24 05:19:51 +0300

    second commit

--- script.js
+++ script.js
@@ -1,20 +1,55 @@
-var pers = new Person("Abdul", 300, 150);
+var pers = new Person("Abdul", 100, 100);
 var wrapper = document.getElementById("wrapper")
 
 function createPers(pers, parent){
+    var healthInner = element("div",
+     {classes:["health-inner", "inner"],
+     style: {
+         width: "100%"
+     }
+    }),
+    staminaInner = element("div",
+     {classes:["stamina-inner", "inner"],
+     style: {
+        width: "100%"
+    }
+    })
     var name = element("div", {classes: ["namePlace"]}), 
-    health = element("div", {classes: ["health-bar"]}),
-    stamina = element("div", {classes: ["stamina-bar"]}),
+    health = element("div", {classes: ["health-bar"]}, [healthInner]),
+    stamina = element("div", {classes: ["stamina-bar"]}, [staminaInner]),
     image = element("div", {classes: ["img"]})
     var card = element("div",
      {
-        classes: ["card-wrapper"]
+        classes: ["card-wrapper"],
     }, 
     [name, health, stamina, image]);
+
+    health.addEventListener("click", function(){
+        pers.health = pers.health - pers.maxHealth * 0.1;
+        var result = (pers.health / pers.maxHealth) * 100;
+        healthInner.style.width = result + "%"
+        if(pers.health == 0){
+            var element = document.getElementById("wrapper")
+            element.parentNode.removeChild(element)
+        }
+    });
+
+    stamina.addEventListener("click", function(){
+        pers.stamina = pers.stamina - pers.maxStamina * 0.1;
+        var result = (pers.stamina / pers.maxStamina) * 100;
+        staminaInner.style.width = result + "%"
+        if(pers.stamina == 0){
+            var element = document.getElementsByClassName("card-wrapper")
+            var style = element[0].style;
+            style.backgroundColor = "gray"
+        }
+    });
 
     parent.append(card)
 }
 
+
 createPers(pers, wrapper);
-createPers(pers, wrapper);
-createPers(pers, wrapper);
+
+
+
